d745078187147dc2c9f2e34d2b5b493196083190
Paul <p.rossiter83@gmail.noreply.github.com>
2025-05-05 17:22:32 +0100

    [Refactored] Clean up CTA component structure and styles for improved readability and consistency

--- src/components/CTA/index.jsx
+++ src/components/CTA/index.jsx
@@ -2,28 +2,27 @@ import Button from "../button";
 import "./index.scss";
 
 const CTA = () => {
-    return (
-        <div className="call-to-action">
-            <div className="text-container">
-                <h2 className="cta-title">Get in Touch</h2>
-                <p className="cta-p">Contact us today to discuss your needs and find out how Morfbot AI Business Solutions can help you achieve your goals.</p>
-            </div>
-            <div className="btn-container">
-                <Button
-                    text="Contact Us"
-                    link="/contact-us"
-                    target="_blank"
-                    background="var(--button)"
-                />
-                <Button
-                    text="Book a Call"
-                    link="https://calendly.com/morfbot/120mins?back=1&month=2024-05"
-                    background="var(--opposite)"
-                    target="_blank"
-                />
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="call-to-action">
+      <div className="text-container">
+        <h2 className="cta-title">
+          Get in <span>Touch</span>
+        </h2>
+        <p className="cta-p">
+          Contact us today to discuss your needs and find out how Morfbot AI
+          Business Solutions can help you achieve your goals.
+        </p>
+      </div>
+      <div className="btn-container">
+        <Button text="Contact Us" link="/contact-us" target="_blank" />
+        <Button
+          text="Book a Call"
+          link="https://calendly.com/morfbot/120mins?back=1&month=2024-05"
+          target="_blank"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default CTA;

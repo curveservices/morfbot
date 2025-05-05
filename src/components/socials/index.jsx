39aba994b55a87bc3904631bf21e8fb29e01e3b8
Paul <p.rossiter83@gmail.noreply.github.com>
2025-05-05 17:23:52 +0100

    Refactor Socials component and styles for improved readability and consistency

--- src/components/socials/index.jsx
+++ src/components/socials/index.jsx
@@ -4,23 +4,22 @@ import { Link } from "react-router-dom";
 import "./index.scss";
 
 const Socials = (props) => {
-    return ( 
-        <>
-            <ul className="iconList">
-                <li className="icons">
-                    <Link to={props.instaLink} target="_blank" rel="noreferer">
-                        <FontAwesomeIcon icon={faInstagram} />
-                    </Link>
-                </li>
-                <li className="icons">
-                    <Link to={props.lilink} target="_blank" rel="noreferer">
-                        <FontAwesomeIcon icon={faLinkedin} />
-                    </Link>
-                </li>
-                
-            </ul>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <ul className="iconList">
+        <li className="icons">
+          <Link to={props.instaLink} target="_blank" rel="noreferer">
+            <FontAwesomeIcon icon={faInstagram} />
+          </Link>
+        </li>
+        <li className="icons">
+          <Link to={props.lilink} target="_blank" rel="noreferer">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </Link>
+        </li>
+      </ul>
+    </>
+  );
+};
+
 export default Socials;
